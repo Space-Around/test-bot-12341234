ba917602d2ef3f423f194eb847d2ce506470b49f
Space Around <viksnamax@mail.ru>
2021-04-03 14:50:42 +0300

    +

--- bot.py
+++ bot.py
@@ -3,8 +3,10 @@ import telebot
 
 bot = telebot.TeleBot(config.TOKEN)
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(content_types=['document'])
 def handle_message(message):
-    bot.send_message(message.chat.id, message.text)
+    # bot.send_message(message.chat.id, message.text)
+    with open('./cancel.png', 'rb') as f1:
+        bot.send_document(message.chat.id, f1)
 
 bot.polling()
